ab4e4a3beab1d1c6aadcc2721d74246309e67538
Zezel-tech22 <lucas.silva@investsmart.com.br>
2025-03-29 16:07:35 -0300

    feat: task two

--- chicago_bikeshare_pt.py
+++ chicago_bikeshare_pt.py
@@ -45,6 +45,9 @@ input("Aperte Enter para continuar...")
 
 print("\nTAREFA 2: Imprimindo o gênero das primeiras 20 amostras")
 
+for i in range(20):
+    sample = data_list[i]
+    print(sample[6])
 
 # Ótimo! Nós podemos pegar as linhas(samples) iterando com um for, e as colunas(features) por índices.
 # Mas ainda é difícil pegar uma coluna em uma lista. Exemplo: Lista com todos os gêneros
